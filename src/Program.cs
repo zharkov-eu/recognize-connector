8fcdc4fc9cfb0278003085d407bd87419c45c768
Evgeni Zharkov <zharkov.ev.u@yandex.ru>
2018-09-24 19:28:43 +0300

    Добавлен пример прямого продукционного вывода

--- src/Program.cs
+++ src/Program.cs
@@ -17,8 +17,17 @@ namespace ExpertSystem
             var rulesGraph = rulesGenerator.GenerateRules(sockets, fieldValues);
             var processor = new Processor(rulesGraph);
 
-            // Пример поиска
+            // Пример обратного продукционного вывода
             var facts = processor.BackProcessing("5145167-4");
+
+            // Пример прямого продукционного вывода
+            Fact numberOfPositions = new Fact("NumberOfPositions", "60");
+            Fact numberOfContacts = new Fact("NumberOfContacts", "120");
+            Fact gender = new Fact("Gender", "Female");
+            var isSocket = processor.ForwardProcessing(
+                new FactSet(numberOfPositions, numberOfContacts, gender),
+                "5145167-4"
+            );
 
             Console.ReadLine();
         }
