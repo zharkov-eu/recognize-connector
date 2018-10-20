51e41fe1a9f8cce19e5a078c11708076e85adf41
Evgeni Zharkov <zharkov.ev.u@yandex.ru>
2018-10-20 09:44:48 +0300

    Добавлен .sh-скрипт для генерации из proto-файлов

--- script/generate_proto.sh
+++ script/generate_proto.sh
@@ -1,1 +1,9 @@
 #!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+SRCDIR="$(readlink -m "${DIR}/../src")"
+
+PROTOC=${HOME}/.nuget/packages/grpc.tools/1.14.1/tools/linux_x64/protoc
+PLUGIN=${HOME}/.nuget/packages/grpc.tools/1.14.1/tools/linux_x64/grpc_csharp_plugin
+
+$PROTOC -I${SRCDIR}/ExpertSystem.Common/Proto --csharp_out ${SRCDIR}/ExpertSystem.Common/Generated  ${SRCDIR}/ExpertSystem.Common/Proto/CustomSocket.proto --grpc_out ${SRCDIR}/ExpertSystem.Common/Generated --plugin=protoc-gen-grpc=$PLUGIN
